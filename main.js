b06798349482895b1327018b770eb63434393b6a
mr-black-8 <boz.black@gmail.com>
2016-07-14 21:50:35 +1000

    things

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ function startStreaming(io) {
     return;
   }
 
-  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "50"];
+  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "50", "-n"];
   proc = spawn('raspistill', args);
 
   console.log('Watching for changes...');
